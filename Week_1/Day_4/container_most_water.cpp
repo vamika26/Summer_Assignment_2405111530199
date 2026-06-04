6d71cbd6cfddb2a85354553cc242e3382b337a1f
vamika26 <vamikajaiswal2605@gmail.com>
2026-06-04 13:32:53 +0530

    week 1 day 4 solutions

---
+++ Week_1/Day_4/container_most_water.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+
+        int maxWater = 0;
+
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+
+            maxWater = max(maxWater, h * w);
+
+            // Move the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+};
