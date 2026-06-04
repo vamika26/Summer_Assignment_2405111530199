13f63f273006af94dfb29186c698a4d7e3d28365
vamika26 <vamikajaiswal2605@gmail.com>
2026-06-04 14:02:54 +0530

    week 1 day 1 solutions

---
+++ Week_1/Day_1/sell_stock.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
